675a5162a5a2841f041ac87055c888f64f2cd4d2
Giulia Padovani <giuliacpadovani@gmail.com>
2020-04-07 21:55:27 -0300

    Add members to classroom WIP

--- laboratorio-api/app/laboratorio/classroom/Classroom.php
+++ laboratorio-api/app/laboratorio/classroom/Classroom.php
@@ -2,6 +2,8 @@
 
 namespace App\laboratorio\classroom;
 
+use App\GitLabUser;
+use App\laboratorio\gitlab\GitLab;
 use App\laboratorio\RemoteRepositoryResolver;
 use App\laboratorio\util\http\ErrorResponse;
 
@@ -15,6 +17,20 @@ class Classroom {
         if($response instanceof ErrorResponse) {
             throw new ClassroomException($response->data['error_message']);
         }
+    }
+
+    public function addMembersByRegistrationCode(array $members_registration_code) {
+        foreach($members_registration_code as $registration_code) {
+            $user = GitLabUser::where('registration_number', $registration_code)->first();
+            if(is_null($user)) {
+                $users_not_found[] = $registration_code;
+                continue;
+            }
+
+            $users_ids[] = $user->getId();
+        }
+
+
     }
 
 }
