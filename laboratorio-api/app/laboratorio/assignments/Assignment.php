b93042a207464e520f1d8636b5355545a607c68a
Giulia Padovani <giuliacpadovani@gmail.com>
2020-06-07 17:26:27 -0300

    Fix create assignments

--- laboratorio-api/app/laboratorio/assignments/Assignment.php
+++ laboratorio-api/app/laboratorio/assignments/Assignment.php
@@ -9,11 +9,13 @@ class Assignment {
     public $name;
     public $description;
     public $classroom_external_id;
+    public $import_from;
 
-    public function __construct(string $name, string $description, string $classroom_external_id) {
+    public function __construct(string $name, ?string $description, string $classroom_external_id, ?string $import_from) {
         $this->name = $name;
         $this->description = $description;
         $this->classroom_external_id = $classroom_external_id;
+        $this->import_from = $import_from;
     }
 
     public static function create(string $provider_access_token, string $name, ?string $description, string $classroom_external_id, string $import_from) {
@@ -28,11 +30,28 @@ class Assignment {
             throw new AssignmentException($response->data['error_message']);
         }
 
-        return new self($response->data->name, $response->data->description, $response->data->id);
+        return new self(
+            $response->data->name,
+            self::getDescription($response->data->description),
+            $response->data->id,
+            self::getImportUrlFromDescription($response->data->description)
+        );
     }
 
     private static function buildDescription(?string $description, string $import_from) {
         return json_encode(['description' => $description, 'import_from' => $import_from]);
+    }
+
+    private static function getImportUrlFromDescription(string $description): ?string {
+        return self::tearDownDescription($description)->import_from;
+    }
+
+    private static function getDescription(string $description): ?string {
+        return self::tearDownDescription($description)->description;
+    }
+
+    private static function tearDownDescription(string $description): \stdClass {
+        return json_decode($description);
     }
 
 }
