1543107765f98e94ac955690c2e66d6ef822e8b2
Giulia Padovani <giuliacpadovani@gmail.com>
2020-08-05 23:23:43 -0300

    Alter structure for users with email and avatar

--- laboratorio-api/app/laboratorio/gitlab/GitUser.php
+++ laboratorio-api/app/laboratorio/gitlab/GitUser.php
@@ -8,19 +8,22 @@ use App\laboratorio\util\http\ErrorResponse;
 class GitUser {
     public $name;
     public $username;
+    public $avatar_url;
 
-    public function __construct(string $name = null, string $username = null) {
+    public function __construct(string $name = null, string $username = null, string $avatar_url = null) {
         $this->name = $name;
         $this->username = $username;
+        $this->avatar_url = $avatar_url;
     }
 
     public function getFromProvider(string $code): ?GitUser {
         $token = GitLabTokenRepository::getToken($code);
         $response = RemoteRepositoryResolver::resolve()->getUserByAccessToken($token);
+
         if($response instanceof ErrorResponse) {
             return null;
         }
 
-        return new self($response->data->name, $response->data->username);
+        return new self($response->data->name, $response->data->username, $response->data->avatar_url);
     }
 }
---
+++ laboratorio-api/database/migrations/2020_08_06_010541_alter_users_add_email.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUsersAddEmail extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table) {
+            $table->string('university_email')->nullable();
+            $table->string('avatar_url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
