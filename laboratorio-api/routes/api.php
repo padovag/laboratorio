d0c30dbe2c0f4a77ecba8f56291411af7df42e00
Giulia Padovani <giuliacpadovani@gmail.com>
2020-08-17 21:17:59 -0300

    Add endpoint to get current

--- laboratorio-api/routes/api.php
+++ laboratorio-api/routes/api.php
@@ -14,6 +14,7 @@
 Route::post('/user/register', 'UserController@register');
 Route::post('/user/auth', 'UserController@authenticate');
 Route::get('/users', 'UserController@list');
+Route::post('/user/current', 'UserController@authenticate');
 
 Route::get('/classroom', 'ClassroomController@list');
 Route::post('/classroom', 'ClassroomController@create');
