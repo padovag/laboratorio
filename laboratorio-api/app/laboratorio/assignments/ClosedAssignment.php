553cf14fa6d1a1864e8c93d09e125b6376cf4cde
Giulia Padovani <giuliacpadovani@gmail.com>
2020-11-26 20:32:24 -0300

    Return student grades when getting students from assignment

--- laboratorio-api/app/laboratorio/assignments/ClosedAssignment.php
+++ laboratorio-api/app/laboratorio/assignments/ClosedAssignment.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class ClosedAssignment extends Model {
     public $fillable = ['external_id', 'user_id', 'classroom_id', 'grade'];
     public $table = 'assignments';
+
+    public static function getByStudent(string $assignment_id, string $student_user_id) {
+        return ClosedAssignment::where(['external_id' => $assignment_id, 'user_id' => $student_user_id])->first();
+    }
 }
